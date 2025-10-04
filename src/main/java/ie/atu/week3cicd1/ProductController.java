c79642380f9cc97c06204ced7ffed45c634386da
Keelan Murphy <g00435965@atu.ie>
2025-10-04 15:57:24 +0100

    Homework task, added @Size so you cant enter too long of a product name, and also added count so it tells you how many products have been entered.

--- src/main/java/ie/atu/week3cicd1/ProductController.java
+++ src/main/java/ie/atu/week3cicd1/ProductController.java
@@ -7,16 +7,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
-@RequestMapping("/product")
+//@RequestMapping means that every endpoint within the controller starts with /product now
+@RequestMapping("/api/products")
 @RestController
 public class ProductController
 {
     List<Product> myList = new ArrayList<>();
 
+    //So this would be /products/hello
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/count")
+    public int countProducts() {
+        return myList.size();
     }
 
 
@@ -26,7 +32,7 @@ public class ProductController
         Product myProduct = new Product("TV", 499);
         return myList;
     }
-
+    //with @PostMapping you can add to the other Mappings
     @PostMapping("/addProduct")
     public Product addProduct(@Valid @RequestBody Product myProduct)
     {
@@ -34,3 +40,4 @@ public class ProductController
         return myProduct;
     }
 }
+//@Valid checks for errors
